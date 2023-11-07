619d24c0503c8aab78c678e5236f158f522b9c35
Chelsea Pang <chelsea.pang96@gmail.com>
2023-11-06 21:50:36 -0800

    Set up firestore configuration and displayed name and email from main.js on profile page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -16,6 +16,7 @@ document.getElementById("profileBtn").onclick = function(){
 
 
 function getNameFromAuth() {
+    console.log("here")
     firebase.auth().onAuthStateChanged(user => {
         // Check if a user is signed in:
         if (user) {
@@ -25,7 +26,8 @@ function getNameFromAuth() {
             userName = user.displayName;
 
             //method #1:  insert with JS
-            document.getElementById("name-goes-here").innerText = userName;    
+            document.getElementById("name-goes-here").innerText = userName;
+            document.getElementById("email-goes-here").innerText = user.email;   
 
             //method #2:  insert using jquery
             //$("#name-goes-here").text(userName); //using jquery
