fe12f6e21e00699efb88ee0a60815cf1c4842281
Jeremy <jarciaga515@gmail.com>
2023-11-14 19:43:08 -0800

    styled plus.html, added js file for photo/review upload function base

---
+++ public/scripts/plus.js
@@ -0,0 +1,93 @@
+document.getElementById("backIcon").onclick = function(){
+    location.href = "../../app/html/main.html"
+}
+document.getElementById("homeBtn").onclick = function(){
+    location.href = "../../app/html/main.html"
+}
+document.getElementById("bookmarkBtn").onclick = function(){
+    location.href = "../../app/html/saved.html"
+}
+document.getElementById("plusBtn").onclick = function(){
+    location.href = "../../app/html/picture.html"
+}
+document.getElementById("searchBtn").onclick = function(){
+    location.href = "../../app/html/search.html"
+}
+document.getElementById("profileBtn").onclick = function(){
+    location.href = "../../app/html/profile.html"
+}
+
+function savePost() {
+    alert ("SAVE POST is triggered");
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            // Do something for the user here. 
+            var desc = document.getElementById("description").value;
+            db.collection("posts").add({
+                owner: user.uid,
+                description: desc,
+                last_updated: firebase.firestore.FieldValue
+                    .serverTimestamp() //current system time
+            }).then(doc => {
+                console.log("1. Post document added!");
+                console.log(doc.id);
+                uploadPic(doc.id);
+            })
+        } else {
+            // No user is signed in.
+            console.log("Error, no user signed in");
+        }
+    });
+}
+
+var ImageFile;
+function listenFileSelect() {
+      // listen for file selection
+      var fileInput = document.getElementById("mypic-input"); // pointer #1
+      const image = document.getElementById("mypic-goes-here"); // pointer #2
+
+			// When a change happens to the File Chooser Input
+      fileInput.addEventListener('change', function (e) {
+          ImageFile = e.target.files[0];   //Global variable
+          var blob = URL.createObjectURL(ImageFile);
+          image.src = blob; // Display this image
+      })
+}
+listenFileSelect();
+
+function uploadPic(postDocID) {
+    console.log("inside uploadPic " + postDocID);
+    var storageRef = storage.ref("images/" + postDocID + ".jpg");
+
+    storageRef.put(ImageFile)   //global variable ImageFile
+       
+                   // AFTER .put() is done
+        .then(function () {
+            console.log('2. Uploaded to Cloud Storage.');
+            storageRef.getDownloadURL()
+
+                 // AFTER .getDownloadURL is done
+                .then(function (url) { // Get URL of the uploaded file
+                    console.log("3. Got the download URL.");
+
+                    // Now that the image is on Storage, we can go back to the
+                    // post document, and update it with an "image" field
+                    // that contains the url of where the picture is stored.
+                    db.collection("posts").doc(postDocID).update({
+                            "image": url // Save the URL into users collection
+                        })
+                         // AFTER .update is done
+                        .then(function () {
+                            console.log('4. Added pic URL to Firestore.');
+                            // One last thing to do:
+                            // save this postID into an array for the OWNER
+                            // so we can show "my posts" in the future
+                            savePostIDforUser(postDocID);
+                        })
+                })
+        })
+        .catch((error) => {
+             console.log("error uploading to cloud storage");
+        })
+}
