95b64d2129cf487a3e5a5af8d22246e61adc871c
Jeremy <jarciaga515@gmail.com>
2023-11-21 16:51:50 -0800

    added functionality of review.html, plus.html

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,7 +5,7 @@ document.getElementById("bookmarkBtn").onclick = function(){
     location.href = "../../app/html/saved.html"
 }
 document.getElementById("plusBtn").onclick = function(){
-    location.href = "../../app/html/picture.html"
+    location.href = "../../app/html/plus.html"
 }
 document.getElementById("searchBtn").onclick = function(){
     location.href = "../../app/html/search.html"
