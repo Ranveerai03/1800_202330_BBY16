1534b809937be6550fb00a7b8cf8ecfb589178f7
Wanyi Zhu <wzhu36@my.bcit.ca>
2023-11-28 13:07:18 -0800

    Updated the profile.html and add function to change the profile image

---
+++ public/scripts/profile.js
@@ -0,0 +1,22 @@
+//------------------------------------------------------------------------------
+// Function to change profile images on profile.html
+//------------------------------------------------------------------------------
+  window.onload = function(){" "}
+  {document
+    .getElementById("imageUpload")
+    .addEventListener("change", function (event) {
+      // Create a new FileReader object
+      const reader = new FileReader();
+      // When the file is read successfully, set it as the source of the profile image
+      reader.onload = function () {
+        // Get the image element by its ID
+        const img = document.getElementById("profileImage");
+        // Set the source of the image element to the file content
+        img.src = reader.result;
+      };
+      // Start reading the first file selected by the user
+      // This will trigger the 'onload' event after the file is read
+      reader.readAsDataURL(event.target.files[0]);
+    })}
+  ;
+
