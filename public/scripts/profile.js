fbbba018f2ddfe8760c655a4e2576be7271fd1cc
Wanyi Zhu <wzhu36@my.bcit.ca>
2023-11-29 09:27:23 -0800

    Displayed name and email on profile.html for login user

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,3 +1,41 @@
+//----------------------------------------------------------
+// Initialize Firebase Auth and get the current user.
+//----------------------------------------------------------
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    insertNameFromFirestore(user);
+    insertEmailFromFirestore(user);
+  } else {
+    // Handle user not logged in
+  }
+});
+
+//----------------------------------------------------------
+// Function to insert the User's Name on this page.
+//----------------------------------------------------------
+function insertNameFromFirestore(user) {
+  db.collection("users").doc(user.uid).get().then(userDoc => {
+      console.log(userDoc.data().name)
+      userName = userDoc.data().name;
+      console.log(userName)
+      document.getElementById("name-goes-here").innerHTML = userName;
+  })
+}
+insertNameFromFirestore();
+
+//----------------------------------------------------------
+// Function to insert the User's Email on this page.
+//----------------------------------------------------------
+function insertEmailFromFirestore(user) {
+  db.collection("users").doc(user.uid).get().then(userDoc => {
+      console.log(userDoc.data().email)
+      userEmail = userDoc.data().email;
+      console.log(userEmail)
+      document.getElementById("email-goes-here").innerHTML = userEmail;
+  })
+}
+insertEmailFromFirestore();
+
 //------------------------------------------------------------------------------
 // Function to change profile images on profile.html
 //------------------------------------------------------------------------------
